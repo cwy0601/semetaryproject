e290ceb653f7426fbb967e3df6f243a528bcd038
ChoiWooYoung <davidki@naver.com>
2016-11-28 03:29:14 -0800

    choose date

---
+++ prog/ch_date/ch_date.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+
+char date[13];
+
+int year;
+int month;
+int day;
+
+int main(void)
+{
+	printf("Please choose date to watch a movie.\n");
+
+	while(1)
+	{
+		scanf("%d", %year);
+
+		if(year > 2017 || year < 2016)
+		{
+			printf("Please choose in 2016 or 2017.\n")
+			continue; 
+		}
+		else
+			break;
+	}
+
+	printf("Please choose a month.\n");
+
+	while(1)
+	{
+		scanf("%d", %month);
+
+		if(month > 12 || month < 0)
+		{
+			printf("Please choose in January ~ December.\n);
+			continue;
+		}
+		else
+			break;
+	}
+
+	printf("Please choose a day.\n");
+
+	while(1)
+	{
+		scanf("%d", %day);
+
+		if(((month <= 7 && month % 2 == 0) || (month > 7 && month % 2 == 1)) && day > 30)
+		{
+			if(month == 2)
+			{
+				if(year % 4 == 0)
+				{
+					printf("Please choose in 0 - 29.\n");
+					continue;
+				}
+				else
+				{
+					printf("Please choose in 0 - 28.\n");
+					continue;
+				}
+			}
+
+			printf("Please choose in 0 - 30.\n");
+			continue;
+		}
+		else if(((month <= 7 && month % 2 == 1) || (month > 7 && month % 2 == 0)) && day > 31)
+		{
+			printf("Please choose in 0 - 31.\n");
+			continue;
+		}
+		else
+			break;
+	}
+
+	printf("You've chosen %d. %d. %d!\n", year, month, day);
+}
