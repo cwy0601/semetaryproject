2aa65288af164650ef8a30d623ca37fbd95cfc32
ChoiWooYoung <davidki@naver.com>
2016-12-05 21:38:17 -0800

    main.c added

---
+++ main/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "info.h"
+
+int main(void)
+{
+	ch_movie();
+	ch_theater();
+	ch_date();
+	ch_seat();
+	payment();
+	view_res();
+
+	return 0;
+}
