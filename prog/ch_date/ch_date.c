fa2c99fbff05495fbb56bf6ddfd579b21c067d45
ChoiWooYoung <davidki@naver.com>
2016-12-07 09:31:45 -0800

    except current day

--- prog/ch_date/ch_date.c
+++ prog/ch_date/ch_date.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 void ch_date(int *year, int *month, int *day)
 {
+	int y_flag = 0;
+	int m_flag = 0;
+
+	time_t timer;
+	struct tm *t;
+
+	timer = time(NULL);
+	t = localtime(&timer);
+
 	printf("Please choose date to watch a movie.\n");
 	printf("Please choose a year (2016 or 2017): ");
 
@@ -16,19 +26,48 @@ void ch_date(int *year, int *month, int *day)
 			printf("Please choose a year again: ");
 			continue;
 		}
-		else
+		else if(*year < (t->tm_year+1900))
+		{
+			printf("Already passed!\n");
+			printf("Please choose a year again: ");
+			continue;
+		}
+		else if(*year > (t->tm_year+1900))
+		{
+			y_flag = 1;
 			break;
-	}
+		}
+		else		
+			break;
+	}	
+
+	printf("Please choose a month : ");
 
 	while (1)
 	{
-		printf("Please choose a month : ");
 		scanf("%d", month);
 
 		if (*month > 12 || *month <= 0)
 		{
 			printf("Please choose in January ~ December.\n");
+			printf("Please choose a month again: ");
 			continue;
+		}
+		else if(y_flag == 0)
+		{
+			if(*month < (t->tm_mon+1))
+			{
+				printf("Already passed!\n");
+				printf("Please choose a month again: ");
+				continue;
+			}
+			else if(*month > (t->tm_mon+1))
+			{
+				m_flag = 1;
+				break;
+			}
+			else
+				break;
 		}
 		else
 			break;
@@ -44,6 +83,15 @@ void ch_date(int *year, int *month, int *day)
 		{
 			printf("Please type a correct day: ");
 			continue;
+		}
+		else if(m_flag == 0)
+		{
+			if(*day < t->tm_mday)
+			{
+				printf("Already passed!\n");
+				printf("Please choose a day again: ");
+				continue;
+			}
 		}
 
 		if (((*month <= 7 && *month % 2 == 0) || (*month > 7 && *month % 2 == 1)) && *day > 30)
